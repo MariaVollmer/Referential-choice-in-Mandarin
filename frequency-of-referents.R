f2cc3d79aaa9de3051925e83010591e9ec7d2437
Maria <74909821+MariaVollmer@users.noreply.github.com>
2022-12-22 18:31:43 +0100

    Update frequency-of-referents.R

--- frequency-of-referents.R
+++ frequency-of-referents.R
@@ -1,14 +1,17 @@
-# This script was written by Maria Vollmer (University of Freiburg, 
-# Australian National University) and Jan Boockmann (University of Bamberg).
-
+# Authors:
+# - Maria Vollmer (University of Freiburg, Australian National University)
+# - Jan Boockmann (University of Bamberg)
+#
 # This script is used in the analysis of the following paper:
 # Vollmer, Maria. Accepted with minor revision. Comparing zero and referential 
 # choice in eight languages with a focus on Mandarin Chinese. Studies in language.
-
+#
 # If you use this script, please make sure to cite the paper above. 
 # If you have any questions, you can contact me via email: mariacvollmer@gmail.com
-
-# This script computes the overall frequency of referents 
+#
+# Description:
+# 
+# computes the overall frequency of referents 
 # which is a variable in the computation of the decision trees.
 
 
