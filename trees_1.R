5a8bc51b3d1fec9e84f22c83e999a57742d64350
Maria <74909821+MariaVollmer@users.noreply.github.com>
2022-12-22 18:37:14 +0100

    Update trees_1.R

--- trees_1.R
+++ trees_1.R
@@ -1,13 +1,15 @@
-## This script was written by Maria Vollmer (University of Freiburg, 
-# Australian National University) and Jan Boockmann (University of Bamberg).
-
+# Authors:
+# - Maria Vollmer (University of Freiburg, Australian National University)
+# - Jan Boockmann (University of Bamberg)
+#
 # This script is used in the analysis of the following paper:
 # Vollmer, Maria. Accepted with minor revision. Comparing zero and referential 
 # choice in eight languages with a focus on Mandarin Chinese. Studies in language.
-
+#
 # If you use this script, please make sure to cite the paper above. 
 # If you have any questions, you can contact me via email: mariacvollmer@gmail.com
-
+#
+# Description:
 ## This script produces the first and third binary classification trees: 
 # testing referential choice between noun phrases, pronouns and zeros 
 # including all variables except person (since nps are always in the third person); 
